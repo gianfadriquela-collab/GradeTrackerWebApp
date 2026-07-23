0b3b2cd364b5fbd8c4b04edb8d9f894ff0911e4a
seanm <sean_ortiz@dlsu.edu.ph>
2026-07-23 20:50:28 +0800

    (fix) minor changes to GradeTrackerWebApp.java

--- src/main/java/ph/edu/dlsu/lbycpob/gradetracker/GradeTrackerWebApp.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetracker/GradeTrackerWebApp.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GradeTrackerWebApp {
 	static void main(String[] args) {
-		SpringApplication.run(GradeTrackerSpringBootApplication.class, args);
+		SpringApplication.run(GradeTrackerWebApp.class, args);
 	}
 }
