33a882b88f88339c0385bb615c4f724091f2b712
johannacastor <johanna_castor@dlsu.edu.ph>
2026-07-23 20:38:16 +0800

    added getters and setters in StudentFormDTO

--- src/main/java/ph/edu/dlsu/lbycpob/gradetracker/dto/StudentFormDTO.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetracker/dto/StudentFormDTO.java
@@ -50,5 +50,36 @@ public class StudentFormDTO {
     @DecimalMax(value = "100.0", message = "Project score must be 0 - 100.")
     private double project;
 
+    public String getName()                  { return name; }
+    public void   setName(String name)       { this.name = name; }
 
+    public String getIdNumber()              { return idNumber; }
+    public void   setIdNumber(String id)     { this.idNumber = id; }
+
+    public double getModule1()               { return module1; }
+    public void   setModule1(double v)       { this.module1 = v; }
+
+    public double getModule2()               { return module2; }
+    public void   setModule2(double v)       { this.module2 = v; }
+
+    public double getModule3()               { return module3; }
+    public void   setModule3(double v)       { this.module3 = v; }
+
+    public double getModule4()               { return module4; }
+    public void   setModule4(double v)       { this.module4 = v; }
+
+    public double getModule5()               { return module5; }
+    public void   setModule5(double v)       { this.module5 = v; }
+
+    public double getClassParticipation()    { return classParticipation; }
+    public void   setClassParticipation(double v) { this.classParticipation = v; }
+
+    public double getTeacherEvaluation()     { return teacherEvaluation; }
+    public void   setTeacherEvaluation(double v)  { this.teacherEvaluation = v; }
+
+    public double getPracticalExam()         { return practicalExam; }
+    public void   setPracticalExam(double v) { this.practicalExam = v; }
+
+    public double getProject()               { return project; }
+    public void   setProject(double v)       { this.project = v; }
 }
